28eaa162ea2c354c3ad74d7965bd0c9811a1f6d1
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-04-10 17:53:33 +0900

    Implement Merge and MergeSort functions with corresponding tests

--- sort/sort.go
+++ sort/sort.go
@@ -1,6 +1,68 @@
 package sort
 
 /*
+MergeSort sort the array arr[p, r)
+worst time complexity: O(nlogn)
+average time complexity: O(nlogn)
+best time complexity: O(nlogn)
+space complexity:
+*/
+func MergeSort(arr []int, p int, r int) {
+	if r-1 <= p {
+		return
+	}
+	q := (p + r) / 2
+	MergeSort(arr, p, q)
+	MergeSort(arr, q, r)
+	// fmt.Println("Merging", arr[p:q], "and", arr[q:r])
+	Merge(arr, p, q, r)
+	// fmt.Println("Merged", arr[p:r])
+}
+
+/*
+Merge merge the two sorted array arr[p, q) and arr[q, r)
+worst time complexity: O(n)
+average time complexity: O(n)
+best time complexity: O(n)
+space complexity:
+*/
+func Merge(arr []int, p int, q int, r int) {
+	i := 0
+	j := 0
+	n1, n2 := q-p, r-q
+	X := make([]int, n1)
+	Y := make([]int, n2)
+	copy(X, arr[p:q])
+	copy(Y, arr[q:r])
+	for {
+
+		if i >= n1 && j >= n2 {
+			break
+		}
+		if i >= n1 {
+			arr[p+i+j] = Y[j]
+			j++
+			continue
+		}
+		if j >= n2 {
+			arr[p+i+j] = X[i]
+			i++
+			continue
+		}
+		if X[i] <= Y[j] {
+			arr[p+i+j] = X[i]
+			i++
+			continue
+		}
+		if Y[j] <= X[i] {
+			arr[p+i+j] = Y[j]
+			j++
+			continue
+		}
+	}
+}
+
+/*
 worst time complexity: O(n^2)
 average time complexity: O(n^2)
 best time complexity: O(1)
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -26,3 +26,33 @@ func TestInsertionSort(t *testing.T) {
 		})
 	}
 }
+
+func TestMerge(t *testing.T) {
+	tests := []struct {
+		input    []int
+		p        int
+		q        int
+		r        int
+		expected []int
+	}{
+		{[]int{1, 4, 6, 7, 2, 3, 8, 9, 5}, 0, 4, 8, []int{1, 2, 3, 4, 6, 7, 8, 9, 5}}}
+	for _, test := range tests {
+		t.Run("Merge", func(t *testing.T) {
+			Merge(test.input, test.p, test.q, test.r)
+			if !reflect.DeepEqual(test.input, test.expected) {
+				t.Errorf("Expected %v, got %v", test.expected, test.input)
+			}
+		})
+	}
+
+}
+func TestMergeSort(t *testing.T) {
+	for _, test := range sortTests {
+		t.Run("MergeSort", func(t *testing.T) {
+			MergeSort(test.input, 0, len(test.input))
+			if !reflect.DeepEqual(test.input, test.expected) {
+				t.Errorf("Expected %v, got %v", test.expected, test.input)
+			}
+		})
+	}
+}
